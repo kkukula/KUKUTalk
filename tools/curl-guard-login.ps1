ebc52b8ab9c7cc29fdcbecf16869a398df8978e7
Krzysztof <krzysztof_kukula@adres.pl>
2025-09-05 12:38:46 +0200

    ops: stable guard login helper (printf + curl --data-binary)

---
+++ tools/curl-guard-login.ps1
@@ -0,0 +1,8 @@
+﻿param()
+$dc="infra/docker/docker-compose.yml"
+$ov="infra/docker/docker-compose.guard.yml"
+$cmd=@"
+printf '%s' '{""username"":""dev1"",""password"":""Passw0rd!""}' >/tmp/login.json
+curl -i -s -H 'Content-Type: application/json' --data-binary @/tmp/login.json http://api:3001/auth/login | sed -n '1,80p'
+"@
+docker compose -f $dc -f $ov exec guard sh -lc $cmd
